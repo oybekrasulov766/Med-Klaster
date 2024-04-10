f0066a4996157f5f1ee4069a954f3e971b598c64
oybekrasulov766 <97724822+oybekrasulov766@users.noreply.github.com>
2024-04-10 15:21:20 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+let dropdown = document.getElementById("dropdown");
+let menu = document.getElementById("menu-media");
+let menu1 = document.getElementById("menu-media1");
+let menu2 = document.getElementById("menu-media2");
+let menu3 = document.getElementById("menu-media3");
+let menu4 = document.getElementById("menu-media4");
+dropdown.addEventListener("click", myFunction => {
+    menu.classList = "block";
+    menu1.classList = "block";
+    menu2.classList = "block";
+    menu3.classList = "block";
+})
+
